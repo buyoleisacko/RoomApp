74aa5b127af0f2e0bc25b8e631dc31ef64c916de
Mac <mac@Macs-MacBook-Pro.local>
2022-01-11 21:08:26 +0300

    added list and add fragment

---
+++ app/src/main/java/com/mindorks/framework/room/fragments/add/AddFragment.kt
@@ -0,0 +1,17 @@
+package com.mindorks.framework.room.fragments.add
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.mindorks.framework.room.R
+
+class AddFragment : Fragment() { override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_add, container, false)
+    }
+}
